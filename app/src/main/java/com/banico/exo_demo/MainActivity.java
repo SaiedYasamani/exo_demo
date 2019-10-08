dd2cf167d1af2a3f9859917a787db101d39b9de8
saeidyasamani <saeidyasamani@gmail.com>
2019-10-08 17:10:55 +0330

    add gesture detection

--- app/src/main/java/com/banico/exo_demo/MainActivity.java
+++ app/src/main/java/com/banico/exo_demo/MainActivity.java
@@ -2,14 +2,11 @@ package com.banico.exo_demo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
-import android.content.Context;
-import android.content.Intent;
 import android.media.AudioManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -17,6 +14,7 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
 import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
@@ -36,9 +34,19 @@ public class MainActivity extends AppCompatActivity {
 
     ProgressBar voiceProgress;
     AudioManager audioManager;
+    GestureDetector gestureDetector;
+    GestureDetector simpleDetector;
     FrameLayout exoLayer;
+    SimpleExoPlayer exoPlayer;
     ImageView speaker;
     boolean isControllerHidden = false;
+
+    enum Direction {
+        up,
+        down,
+        left,
+        right
+    }
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -51,33 +59,114 @@ public class MainActivity extends AppCompatActivity {
         speaker = findViewById(R.id.speaker);
         ImageButton setting = findViewById(R.id.setting);
         voiceProgress = findViewById(R.id.voice_progress);
+        voiceProgress.setMax(100);
+        voiceProgress.setProgress(50);
         audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        gestureDetector = new GestureDetector(this, new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                return true;
+            }
 
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                if(isControllerHidden){
+                    playerView.showController();
+                    isControllerHidden = false;
+                }else {
+                    playerView.hideController();
+                    isControllerHidden = true;
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float distanceX, float distanceY) {
+                double angle = getAngle(motionEvent.getX(), motionEvent.getY(), motionEvent1.getX(), motionEvent1.getY());
+                Direction direction = fromAngle(angle);
+                switch (direction) {
+                    case up:
+                        voiceProgress.setVisibility(View.VISIBLE);
+                        speaker.setVisibility(View.VISIBLE);
+                        voiceProgress.setProgress(voiceProgress.getProgress() + 1);
+                        exoPlayer.setVolume(exoPlayer.getVolume() + 0.01f);
+                        Toast.makeText(MainActivity.this, "volume: " +  exoPlayer.getVolume(), Toast.LENGTH_LONG).show();
+                        break;
+                    case down:
+                        voiceProgress.setVisibility(View.VISIBLE);
+                        speaker.setVisibility(View.VISIBLE);
+                        voiceProgress.setProgress(voiceProgress.getProgress() - 1);
+                        exoPlayer.setVolume(exoPlayer.getVolume() - 0.01f);
+                        Toast.makeText(MainActivity.this, "volume: " +  exoPlayer.getVolume(), Toast.LENGTH_LONG).show();
+                        break;
+                    case right:
+                        Toast.makeText(MainActivity.this, "go right", Toast.LENGTH_LONG).show();
+                        break;
+                    case left:
+                        Toast.makeText(MainActivity.this, "go left", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float velocityX, float velocityY) {
+                return false;
+            }
+        });
+        simpleDetector = new GestureDetector(this,new GestureDetector.SimpleOnGestureListener(){
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                double angle = getAngle(e1.getX(), e1.getY(), e2.getX(), e2.getY());
+                Direction direction = fromAngle(angle);
+                switch (direction) {
+                    case up:
+                        voiceProgress.setVisibility(View.VISIBLE);
+                        speaker.setVisibility(View.VISIBLE);
+                        voiceProgress.setProgress(voiceProgress.getProgress() + 1);
+                        exoPlayer.setVolume(exoPlayer.getVolume() + 0.1f);
+                        Toast.makeText(MainActivity.this, "go up", Toast.LENGTH_LONG).show();
+                        break;
+                    case down:
+                        voiceProgress.setVisibility(View.VISIBLE);
+                        speaker.setVisibility(View.VISIBLE);
+                        voiceProgress.setProgress(voiceProgress.getProgress() - 1);
+                        exoPlayer.setVolume(exoPlayer.getVolume() - 0.1f);
+                        Toast.makeText(MainActivity.this, "go down", Toast.LENGTH_LONG).show();
+                        break;
+                    case right:
+                        Toast.makeText(MainActivity.this, "go right", Toast.LENGTH_LONG).show();
+                        break;
+                    case left:
+                        Toast.makeText(MainActivity.this, "go left", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return true;
+            }
+        });
         ImageView brightness = findViewById(R.id.brightness);
         brightness.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                boolean settingsCanWrite = hasPermission(MainActivity.this);
-                if(!settingsCanWrite) {
-                    changePermission(MainActivity.this);
-                }else {
-//                    WindowManager.LayoutParams attributes = getWindow().getAttributes();
-//                    attributes.screenBrightness += 10;
-//                    getWindow().setAttributes(attributes);
-//                    getWindow().addFlags(WindowManager.LayoutParams.FLAGS_CHANGED);
-
-                    // Change the screen brightness change mode to manual.
-                    Settings.System.putInt(MainActivity.this.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS_MODE, Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL);
-                    // Apply the screen brightness value to the system, this will change the value in Settings ---> Display ---> Brightness level.
-                    // It will also change the screen brightness for the device.
-                    Settings.System.putInt(MainActivity.this.getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 255);
-                }
+                WindowManager.LayoutParams attributes = getWindow().getAttributes();
+                attributes.screenBrightness = 0F;
+                getWindow().setAttributes(attributes);
+                getWindow().addFlags(WindowManager.LayoutParams.FLAGS_CHANGED);
             }
         });
 
         final DefaultTrackSelector trackSelector = new DefaultTrackSelector(new AdaptiveTrackSelection.Factory());
-        final SimpleExoPlayer exoPlayer = ExoPlayerFactory
+        exoPlayer = ExoPlayerFactory
                 .newSimpleInstance(this, new DefaultRenderersFactory(this), trackSelector);
         DefaultHttpDataSourceFactory baseFactory = new DefaultHttpDataSourceFactory(Util.getUserAgent(this, getPackageName()), DefaultHttpDataSource.DEFAULT_CONNECT_TIMEOUT_MILLIS, DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS, false);
         String url = "https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8";
@@ -91,18 +180,9 @@ public class MainActivity extends AppCompatActivity {
         exoLayer.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN && isControllerHidden){
-                    playerView.showController();
-                    isControllerHidden = false;
-                }else if(motionEvent.getAction() == MotionEvent.ACTION_DOWN && !isControllerHidden){
-                    playerView.hideController();
-                    isControllerHidden = true;
-                }
-                if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-                    playerView.hideController();
-                    AdjustVolume(motionEvent);
-                }
+                gestureDetector.onTouchEvent(motionEvent);
                 if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    voiceProgress.setVisibility(View.GONE);
                     speaker.setVisibility(View.GONE);
                 }
                 return true;
@@ -125,6 +205,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        exoPlayer.setVolume(0.5f);
         setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -133,27 +214,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void changePermission(MainActivity mainActivity) {
-        Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
-        mainActivity.startActivity(intent);
+    private double getAngle(float x, float y, float x2, float y2) {
+        double rad = Math.atan2(y-y2,x2-x) + Math.PI;
+        return (rad*180/Math.PI + 180)%360;
     }
 
-    private boolean hasPermission(MainActivity mainActivity) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return Settings.System.canWrite(mainActivity);
+    public static Direction fromAngle(double angle) {
+        Log.d("anggle","angel: " + angle);
+        if (inRange(angle, 45, 135)) {
+            return Direction.up;
+        } else if (inRange(angle, 0, 45) || inRange(angle, 315, 360)) {
+            return Direction.right;
+        } else if (inRange(angle, 225, 315)) {
+            return Direction.down;
+        } else {
+            return Direction.left;
         }
-        return false;
     }
 
-    private void AdjustVolume(MotionEvent motionEvent) {
-        speaker.setVisibility(View.VISIBLE);
-        voiceProgress.setMax(exoLayer.getHeight());
-        voiceProgress.setProgress((int) motionEvent.getAxisValue(MotionEvent.AXIS_Y));
-        float current = motionEvent.getAxisValue(MotionEvent.AXIS_Y);
-        float whole = exoLayer.getHeight();
-        float percent = (current / whole) * 100;
-        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, (int) ((percent * maxVolume) / 100), AudioManager.FLAG_PLAY_SOUND);
-        Log.d("volume", "onTouch: " + audioManager.getStreamVolume(AudioManager.STREAM_MUSIC));
+    private static boolean inRange(double angle, int start, int end) {
+        return angle >= start && angle < end;
     }
 }
